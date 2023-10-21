8f3667f370e025af97ebacf3d362746143c7548a
lukas0409 <112852022+lukas0409@users.noreply.github.com>
2023-10-21 18:03:55 +0900

    ✨ gundambase_lcd.ino 파일 업데이트
    gundambase_lcd.ino 파일 업데이트
    
    업데이트 내용
    - 주석 추가

--- code/gundambase_lcd.ino
+++ code/gundambase_lcd.ino
@@ -4,15 +4,17 @@
   - 자세한 설명은 동봉된 한글 파일 참조
 */
 
-#include <LiquidCrystal_I2C.h> // 
+#include <LiquidCrystal_I2C.h> // I2C LCD 라이브러리
 #include <Wire.h> // LCD 2004 12C
 
 LiquidCrystal_I2C lcd (0x27,20,4); // 접근주소: 0x3F or 0x27
 
+// 버튼 2개 변수
 int readValue7 = 0;
 int readValue8 = 0;
 
 void setup() {
+  // pinMode 설정
   pinMode(7, INPUT);
   pinMode(8, INPUT);
   lcd.init(); // LCD 초기화
@@ -80,6 +82,7 @@ void loop() {
   button_check();
 }
 
+// 문 열림 표출 함수
 void door_openning() {
   lcd.init();
   lcd.setCursor (0, 0); // 두번째 줄 문자열 출력
@@ -88,6 +91,7 @@ void door_openning() {
   readValue8 = 0;
 }
 
+// 문 닫힘 표출 함수
 void door_closing() {
   lcd.init();
   lcd.setCursor (0, 0); // 두번째 줄 문자열 출력
@@ -96,14 +100,18 @@ void door_closing() {
   readValue7 = 0;
 }
 
+// 버튼 상태확인 후 함수연계
 void button_check() {
+  // 버튼값 동기화
   readValue7 = digitalRead(7);
   readValue8 = digitalRead(9);
 
+  // 문 열림문구 실행 버튼 인식
   if (readValue8 == 1) {
     door_openning();
   }
 
+  // 문 닫힘문구 실행 버튼 인식
   if (readValue7 == 1) {
     door_closing();
   }
